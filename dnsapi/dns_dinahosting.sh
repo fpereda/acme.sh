2b3b69f30bda71cac57a39713581e98caff97b07
Fernando J. Pereda <fpereda@gmail.com>
2020-01-18 00:10:43 +0100

    dns_dinahosting.sh: support different domains
    The earlier implementation of having DINAHOSTING_DomainName was a hack. This uses the dinahosting api to see which domains are available for the user and see what's the hostname for the fulldomain.

--- dnsapi/dns_dinahosting.sh
+++ dnsapi/dns_dinahosting.sh
@@ -6,9 +6,15 @@ dns_dinahosting_add() {
   fulldomain=$1
   txtvalue=$2
   _dinahosting_check_and_set
+  hostname="$(_dinahosting_get_hostname "$fulldomain")"
+  if [ -z "$hostname" ] ; then
+    _err "Could not parse $fulldomain into hostname and domainname."
+    return 1
+  fi
 
-  hostname="${fulldomain%%.$DINAHOSTING_DomainName}"
-  _dina_rest "$(_dinahosting_addtxt "$hostname" "$txtvalue")"
+  domainname="${fulldomain#$hostname.}"
+  _debug "hostname=$hostname domainname=$domainname"
+  _dina_rest "$(_dinahosting_addtxt "$hostname" "$domainname" "$txtvalue")"
 }
 
 
@@ -18,33 +24,50 @@ dns_dinahosting_rm() {
   fulldomain=$1
   txtvalue=$2
   _dinahosting_check_and_set
+  hostname="$(_dinahosting_get_hostname "$fulldomain")"
+  if [ -z "$hostname" ] ; then
+    _err "Could not parse $fulldomain into hostname and domainname."
+    return 1
+  fi
 
-  hostname="${fulldomain%%.$DINAHOSTING_DomainName}"
-  _dina_rest "$(_dinahosting_deletetxt "$hostname" "$txtvalue")"
+  domainname="${fulldomain#$hostname.}"
+  _dina_rest "$(_dinahosting_deletetxt "$hostname" "$domainname" "$txtvalue")"
+}
+
+# Usage: fulldomain
+_dinahosting_get_hostname() {
+  fulldomain=$1
+  _dina_rest "$(_dinahosting_base "Services_GetDomains")"
+  printf "%s" "$response" \
+    | sed -n -e "/^.*_domain = '\([^']*\)'$/s--\1-gp" \
+    | while read registereddomain ; do
+        parsedhostname="${fulldomain%.$registereddomain}"
+        if [ "$parsedhostname" != "$fulldomain" ] ; then
+          echo -e "$parsedhostname"
+        fi
+      done
+  return 0
 }
 
 _dinahosting_check_and_set() {
-    DINAHOSTING_Username="${DINAHOSTING_Username:-$(_readaccountconf_mutable DINAHOSTING_Username)}"
+  DINAHOSTING_Username="${DINAHOSTING_Username:-$(_readaccountconf_mutable DINAHOSTING_Username)}"
   DINAHOSTING_Password="${DINAHOSTING_Password:-$(_readaccountconf_mutable DINAHOSTING_Password)}"
-  DINAHOSTING_DomainName="${DINAHOSTING_DomainName:-$(_readaccountconf_mutable DINAHOSTING_DomainName)}"
-  if [ -z "$DINAHOSTING_Username" ] || [ -z "$DINAHOSTING_Password" ] || [ -z "$DINAHOSTING_DomainName" ]; then
+  if [ -z "$DINAHOSTING_Username" ] || [ -z "$DINAHOSTING_Password" ]; then
     DINAHOSTING_Username=""
     DINAHOSTING_Password=""
-    DINAHOSTING_DomainName=""
-    _err "Please specify DinaHosting username, password and domain name."
+    _err "Please specify DinaHosting username and password."
     return 1
   fi
 
   #save the credentials to the account conf file.
   _saveaccountconf_mutable DINAHOSTING_Username "$DINAHOSTING_Username"
   _saveaccountconf_mutable DINAHOSTING_Password "$DINAHOSTING_Password"
-  _saveaccountconf_mutable DINAHOSTING_DomainName "$DINAHOSTING_DomainName"
 }
 
 # Usage: url
 _dina_rest() {
   url=$1
-  echo _debug "url=$url"
+  _debug "url=$url"
 
   response=$(_get "$url")
 
@@ -64,23 +87,25 @@ _dina_rest() {
 # Usage: command
 _dinahosting_base() {
   command=$1
-  echo -e "https://dinahosting.com/special/api.php?AUTH_USER=$DINAHOSTING_Username&AUTH_PWD=$DINAHOSTING_Password&responseType=Simple&domain=$DINAHOSTING_DomainName&orderBy=none&command=$command"
+  echo -e "https://dinahosting.com/special/api.php?AUTH_USER=$DINAHOSTING_Username&AUTH_PWD=$DINAHOSTING_Password&responseType=Simple&command=$command"
 }
 
-# Usage: hostname txtvalue
+# Usage: hostname domainname txtvalue
 _dinahosting_addtxt() {
   hostname=$1
-  txtvalue=$2
+  domainname=$2
+  txtvalue=$3
   command="Domain_Zone_AddTypeTXT"
-  echo -e "$(_dinahosting_base "$command")&hostname=$hostname&text=$txtvalue"
+  echo -e "$(_dinahosting_base "$command")&orderBy=none&domain=$domainname&hostname=$hostname&text=$txtvalue"
 }
 
-# Usage: hostname txtvalue
+# Usage: hostname domainname txtvalue
 _dinahosting_deletetxt() {
   hostname=$1
-  txtvalue=$2
+  domainname=$2
+  txtvalue=$3
   command="Domain_Zone_DeleteTypeTXT"
-  echo -e "$(_dinahosting_base "$command")&hostname=$hostname&value=$txtvalue"
+  echo -e "$(_dinahosting_base "$command")&orderBy=none&domain=$domainname&hostname=$hostname&value=$txtvalue"
 }
 
 # vim: set sts=2 ts=2 sw=2 et:
